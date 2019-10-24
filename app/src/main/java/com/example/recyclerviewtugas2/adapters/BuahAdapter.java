c6265dc1e4546ee3b166e5db0a6fa3d12aa77f14
septakusumaningtyas <septakusumaningtyas@gmail.com>
2019-10-24 16:01:27 +0700

    Project 2

---
+++ app/src/main/java/com/example/recyclerviewtugas2/adapters/BuahAdapter.java
@@ -0,0 +1,62 @@
+package com.example.recyclerviewtugas2.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.recyclerviewtugas2.R;
+import com.example.recyclerviewtugas2.models.Buah;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BuahAdapter extends RecyclerView.Adapter<BuahAdapter.MyViewHolder>{
+    //1 dataset
+    private List<Buah> listBuah = new ArrayList<>();
+
+    //2 constructor
+    public BuahAdapter(List<Buah> listBuah) {
+        this.listBuah = listBuah;
+    }
+
+    @NonNull
+    @Override
+    public BuahAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View vh = LayoutInflater.
+                from(viewGroup.getContext()).
+                inflate(R.layout.item_buah,viewGroup,false);
+
+        //4 membuat view holder
+        MyViewHolder viewHolder = new MyViewHolder(vh);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BuahAdapter.MyViewHolder holder, int position) {
+        Buah item = listBuah.get(position);
+        holder.txtBuah.setText(item.getJudul());
+        holder.imageBuah.setImageDrawable(item.getImage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listBuah.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        public ImageView imageBuah;
+        public TextView txtBuah;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            //6 menghubungkan id di layout ke view holder
+            imageBuah = itemView.findViewById(R.id.image_buah);
+            txtBuah = itemView.findViewById(R.id.txt_buah);
+        }
+    }
+}
